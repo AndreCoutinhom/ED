a374bc77ee31b5eead2ea5f33f70b6d9be607e53
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2026-02-24 11:16:22 -0300

    Add files via upload

---
+++ aula_1/src/Projeto_Revisao_ED/TesteVetor.java
@@ -0,0 +1,28 @@
+package Projeto_Revisao_ED;
+
+public class TesteVetor {
+    public static void main(String[] args) throws Exception {
+        Vetor vetor = new Vetor(5); // Criamos um vetor com 4 espaços disponíveis para strings.
+
+        // Adicionamos os elementos.
+        vetor.adiciona("Água");
+        vetor.adiciona("Terra");
+        vetor.adiciona("Fogo");
+        vetor.adiciona("Ar");
+
+        System.out.println("Os elementos da natureza pela ordem da lenda de Aang: " + vetor.toString());
+
+        // Busca pelo primeiro método
+        System.out.println("Buscando pelo quarto elemento: " + vetor.busca(3));
+        // A chamada abaixo é para testar erros. Qualquer comando depois dela não será executado se ela for.
+        // System.out.println("Buscando pelo quinto elemento: " + vetor.busca(5));
+
+        // Busca pelo segundo método
+        System.out.println("Buscando pela posição da água: " + vetor.busca1("Água"));
+
+        vetor.adicionaInicio(0, "Avatar");
+
+        System.out.println("Depois que o Aang chegou: " + vetor.toString());
+
+    }
+}
---
+++ aula_1/src/Projeto_Revisao_ED/Vetor.java
@@ -0,0 +1,108 @@
+package Projeto_Revisao_ED;
+
+import java.util.Arrays;
+
+public class Vetor {
+    public String[] elementos;
+
+    public int tamanho;
+
+    // TENTATIVA 1: Método construtor, para instanciar objetos declarando-lhes um valor padrão.
+    /*
+    // No método abaixo, a capacidade de armazenamento do vetor está associada ao vetor de elementos.
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+
+    // Com o laço de repetição, estamos criando uma estrutura baseada em uma fila de strings. Cada string é adicionada ao vetor.
+    public void adiciona(String elemento) {
+        for(int i = 0; i < this.elementos.length; i++) {
+            if (this.elementos[i] == null) { // Usamos o null porque assim é como o Java reconhece um espaço de string vazio. Se tiver um espaço vazio, então um novo elemento é adicionado àquele espaço.
+                this.elementos[i] = elemento;
+                break;
+            }
+        }
+    }
+    // Esse método funciona, mas é ineficiente porque percorre todo o vetor todas as vezes.
+
+
+ */
+    // TENTATIVA 2: O método abaixo é um pouco mais eficiente porque trabalha, exclusivamente, por condições.
+    // Ele não faz verificações de valor nulo. Ele parte de um tamanho definido e adiciona conforme a disponibilidade do espaço.
+    // Quando não há mais espaços disponíveis, ele abre uma exceção para avisar ao usuário.
+
+    public Vetor (int capacidade) {
+        this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+    public void adiciona (String elemento) throws Exception {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+        } else {
+            throw new Exception("O vetor ja esta cheio " + "nao e possivel adicionar novos elementos!");
+        }
+    }
+
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    @Override // Para mudar parâmetros de um método especificamente em uma classe. Ideal para mudar métodos de classes importadas.
+    // Usamos o Override para somente mostrar os elementos que estão incluídos no tamanho do vetor, e não em sua capacidade. Dessa forma, o valor null não será printado.
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("");
+
+        for (int i = 0; i < this.tamanho-1; i++) {
+            s.append(this.elementos[i]);
+            s.append("; ");
+        }
+
+        if(this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho - 1]);
+        }
+
+        s.append(".");
+
+        return s.toString();
+    }
+
+    // MÉTODOS DE BUSCA
+
+    // O primeiro método busca o número da posição como parâmetro e retorna a string do elemento.
+
+    public String busca(int posicao) throws Exception {
+        if (posicao >= 0 && posicao < tamanho) {
+            return this.elementos[posicao];
+        } else {
+            throw new Exception("Calma aí Lux Elementalista! Só existem 4!");
+        }
+    }
+
+    // O segundo método de busca faz o contrário. Inserimos uma string como parâmetro e o código retorna a posição em que está.
+    public int busca1(String elemento) {
+        for (int i = 0; i < tamanho; i++) {
+            if (elementos[i].equals(elemento)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean adicionaInicio (int posicao, String elemento) throws Exception {
+        if (posicao >= 0 && posicao < tamanho) {
+            for (int i = this.tamanho - 1; i >= posicao; i--) {
+                this.elementos[i + 1] = this.elementos[i];
+            }
+            this.elementos[posicao] = elemento;
+            this.tamanho++;
+        } else {
+            throw new Exception ("Posicao Invalida!");
+        }
+        return true;
+    }
+
+}
+
