a65b7d3e8db23df0479ce2d636fdecd194f54331
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2026-03-31 10:44:05 -0300

    Add files via upload

---
+++ Listas/src/lista/Lista.java
@@ -0,0 +1,34 @@
+package lista;
+
+import java.util.List;
+
+public class Lista {
+
+    public static void main(String[] args) {
+        ListaEncadeada<String> lista = new ListaEncadeada<>();
+
+        System.out.println("Tamanho da lista = " + lista.getTamanho());
+
+        lista.adiciona("Claudio");
+
+        System.out.println("Tamanho da Lista = " + lista.getTamanho());
+        System.out.println("Início da Lista = " + lista.getInicio().getElemento());
+        System.out.println("Fim da Lista = " + lista.getFim().getElemento());
+
+        lista.adiciona("Camila");
+
+        System.out.println(lista);
+        System.out.println("Tamanho da Lista = " + lista.getTamanho());
+        System.out.println("Início da Lista = " + lista.getInicio().getElemento());
+        System.out.println("Fim da Lista = " + lista.getFim().getElemento());
+
+        System.out.println("Elemento na posição 2 = " + lista.get(2).getElemento());
+
+        lista.remover("Claudio");
+
+        System.out.println("Tamanho da Lista = " + lista.getTamanho());
+        System.out.println("Início da Lista = " + lista.getInicio().getElemento());
+        System.out.println("Fim da Lista = " + lista.getFim().getElemento());
+
+    }
+}
